4a2572044ab11d6932c4a5a4a401e3e8952a8fe0
arnavmanesh <arnavmanesh@gmail.com>
2025-07-10 11:16:17 +0530

    image path fix

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -19,7 +19,7 @@ export const Auth = ({ setIsAuth }) => {
   return (
     <div className="auth-container">
       <div className="login-card">
-<img src="/logo512.png" alt="QuickChat Logo" className="login-logo" />
+<img src="logo512.png" alt="QuickChat Logo" className="login-logo" />
         <h1>Quick Chat</h1>
         <p>The easiest way to chat with people all around the world.</p>
         <button onClick={signInWithGoogle} className="google-btn" >
